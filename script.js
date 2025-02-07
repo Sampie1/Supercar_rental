6cec2968625391b6961d7320e2e669265225ddc0
Sam <101374@glr.nl>
2025-02-07 20:39:55 +0100

    Aantal aanpassingen

--- script.js
+++ script.js
@@ -21,3 +21,36 @@ sr.reveal(".header__container img", {
     ...scrollRevealOption,
     delay: 1000,
 });
+
+//Optie functie voor steden
+function updateSteden(){
+    const steden = {
+        "drenthe": ["Assen", "Emmen", "Hoogeveen", "Meppel"],
+        "flevoland": ["Almere", "Lelystad", "Dronten", "Zeewolde"],
+        "friesland": ["Leeuwarden", "Sneek","Drachten", "Harlingen"],
+        "gelderland": ["Arnhem", "Nijmegen", "Apeldoorn", "Ede"],
+        "groningen": ["Groningen", "Delfzijl", "Winsum", "Veendam"],
+        "limburg": ["Maastricht", "Venlo", "Roermond", "Heerlen"],
+        "noord-brabant": ["Eindhoven", "Tilburg", "'s-Hertogenbosch", "Breda"],
+        "noord-holland": ["Amsterdam", "Haarlem", "Alkmaar", "Hoorn"],
+        "overijssel": ["Zwolle", "Enschede", "Deventer", "Almelo"],
+        "utrecht": ["Utrecht", "Amersfoort", "Zeist", "Nieuwegein"],
+        "zeeland": ["Middelburg", "Vlissingen", "Goes", "Terneuzen"],
+        "zuid-holland": ["Rotterdam", "Den Haag", "Leiden", "Delft"]
+    };
+    let provincie = document.getElementById("provincie").value;
+    let stadSelect = document.getElementById("stad");
+
+    //leeg maken van huidige opties
+    stadSelect.innerHTML = '<option value="">-- Selecteer een stad --</option>';
+
+    //toevoegen van nieuwe opties als er een geldige provincie is gekozen
+    if (provincie in steden) {
+        steden [provincie].forEach(stad => {
+            let option = document.createElement("option");
+            option.value = stad.toLowerCase();
+            option.textContent = stad;
+            stadSelect.appendChild(option);
+        });
+    }
+}
